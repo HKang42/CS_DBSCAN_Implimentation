06ba2d707831f680221abe9cad8630c4bb680754
hkang42 <h.kang.q@gmail.com>
2020-06-25 00:43:03 -0400

    Internet fixed. Working implimentation of DBSCAN

--- Cluster_class.py
+++ Cluster_class.py
@@ -1,22 +1,100 @@
 import numpy as np
 
-class Cluster(tuple):
-    def __init__(self, data = None, labels = None):
+class Cluster():
+    def __init__(self, data = None, labels = None, index = None):
         self.data = data
         if self.data.all() == None:
             self.data = np.array([])
         
         self.labels = [0] * len(self.data)
-    
+
+        self.index = [i for i in range(0, len(data))]
+
     def __str__(self):
-        print_str = ''
+        print_str = "{:>5}{:>12}{:>11}\n".format("Index", "Value", "Label")
         for i in range(len(self.data)):
-            print_str += "{}\t{:>4}\n".format(str(self.data[i]), str(self.labels[i]))
+            print_str += "{:>5}{:>12}{:>11}\n".format(str(self.index[i]), str(self.data[i]), str(self.labels[i]))
         return print_str
 
 
+    def __getitem__(self, i):
+        return self.data[i]
 
-# Array = np.array([[0,0], [0,1], [0,2], [1,1], [5,1], [9,1], [10,0], [10,1], [10,2]])
-# clusters = Cluster(Array)
 
-# print(clusters)
+    def __setitem__(self, i, data_val):
+        """
+        Set the data value and, optionally, the label for a given index.
+        Input should be a numpy array or a tuple of an array and an integer.
+        If a label is not provided, the default value of 0 is used.
+        """
+        if type(data_val) is tuple:
+            
+            if type(data_val[0]) is not np.ndarray:
+                raise TypeError(f"Expected type {np.ndarray}, got {type(data_val[0])} instead.")
+            self.data[i] = data_val[0]
+
+            if (type(data_val[1]) is not int) and (type(data_val[1]) is not str):
+                raise TypeError(f"Expected type {int} or {str}, got {type(data_val[1])} instead.")
+            self.labels[i] = data_val[1]
+
+        else:
+            if type(data_val) is not np.ndarray:
+                raise TypeError(f"Expected type {np.ndarray} or {tuple}, got {type(data_val[0])} instead.")
+            self.data[i] = data_val
+            self.labels[i] = 0
+
+
+    def set_label(self, value, label, assign = 'All'):
+        """
+        Set the label for a given index or data value. 
+        If multiple points have the same value, the method defaults to setting the label for each one.
+        This can be overridden by setting the 'assign' parameter to integer, n. 
+        The method will then assign only the first n points with the same value.
+        """
+        if type(value) is int:
+            self.labels[value] = label
+
+        elif type(value) is np.ndarray:
+
+            # Get the index for every data value that matches the input value
+            indices = np.all(self.data == value, axis=1).nonzero()[0]
+
+            if assign == 'All':
+                for i in indices:
+                    self.labels[i] = label
+            
+            else:
+                i = 0
+                while i < assign and i < len(indices):
+                    self.labels[i] = label
+                    i+=1
+
+        else:
+            raise TypeError(f"Expected type {np.ndarray} or {int}, got {type(value)} instead.")
+
+
+if __name__ == "__main__": 
+
+    # Create Object
+    Array = np.array([[0,0], [0,1], [0,2], [1,1], [5,1], [9,1], [10,0], [10,1], [10,2]])
+    clusters = Cluster(Array)
+
+    # Test print function
+    print(clusters)
+
+
+    # # Test __setitem__
+    # clusters[0] = Array[8], '1'
+    # print(clusters)
+
+    # Test set_label 
+    clusters.set_label(Array[8], 'test 1')
+    print(clusters)
+
+    clusters[3] = Array[8]
+    clusters[4] = Array[8]
+    clusters[5] = Array[8]
+
+    clusters.set_label(Array[8], 'test 2')
+    print(clusters)
+
--- DBSCAN.py
+++ DBSCAN.py
@@ -9,27 +9,6 @@ from Cluster_class import Cluster
 import numpy as np
 import matplotlib.pyplot as plt
 
-def get_distances(point, arr, epsilon):
-    """
-    Given a point and an n x m array, calculate the distance between that point and every other point in the array
-    Record the distances in a new n x 2 array. 
-    [n, 0] contains the distance value. 
-    [n, 1] is 1 if the distance is less or equal to epsilon (is a connection) and 0 otherwise.
-
-    Returns a tuple where the first entry is the array of distances and the second is the number of connections
-    """
-    distances = np.zeros((len(arr), 2))
-    neighbor = 0
-    for i in range(len(arr)):
-        #print("a", point, "b",arr[i], "c",np.linalg.norm(point - arr[i]))
-        dist = np.linalg.norm(point - arr[i])
-        distances[i, 0] = dist
-        
-        if dist <= epsilon:
-            distances[i, 1] = 1
-            neighbor += 1
-
-    return distances, neighbor
 
 """
 Build model:
@@ -52,21 +31,85 @@ loop through Array
         
 """
 
+def get_distances(point, arr, epsilon):
+    """
+    Given a point and an n x m array, calculate the distance between that point and every other point in the array
+    Record the distances in a new n x 2 array. 
+    [n, 0] contains the distance value. 
+    [n, 1] is 1 if the distance is less or equal to epsilon (is a connection) and 0 otherwise.
+
+    Returns a tuple where the first entry is the array of distances and the second is the number of connections
+    """
+    distances = np.zeros((len(arr), 2))
+    neighbor = 0
+    for i in range(len(arr)):
+        #print("a", point, "b",arr[i], "c",np.linalg.norm(point - arr[i]))
+        dist = np.linalg.norm(point - arr[i])
+        distances[i, 0] = dist
+        
+        if dist <= epsilon:
+            distances[i, 1] = 1
+            neighbor += 1
+
+    return distances, neighbor
+
+
+def create_cluster(point, arr, epsilon, min_points, cluster, c):
+    """
+    Recursively grow a cluster given a starting point, an array, a max distances, and a minimum number of points.
+    Modifies the input cluster object by setting all points within the cluster to c.
+    """
+    distances, connections = get_distances(point, arr, epsilon)
+
+
+    # Every time we call this function, we add the point to the cluster
+    cluster.set_label(point, c)
+
+
+    # Recursion base case, we have run out of connecting points (reach a terminating point/leaf)
+    if connections == 0:
+        return 
+
+    # If we are not at base case:
+    # Continue jumping to connecting points and labeling c.
+    # Each time we jump, we shorten the input array by
+    # removing the connecting points from the input array.
+
+
+    # Filter the array down to only connecting points
+    # We use the fact that the indices for arr and distances correpsond to the same points
+    connecting_points = arr[ distances[:,1]==1 ]
+
+
+    # We generate the new input array.
+    # This is the array of points minus the connecting points (includes the original point itself)
+    # We must subtract all connecting points instead of just the inpout point to prevent points from 
+    # connecting back and forth with each other
+    new_arr = arr[ distances[:,1]==0 ]
+
+
+    for p in connecting_points:
+        
+        create_cluster(p, new_arr, epsilon, min_points, cluster, c)
+        
+    return None
 
 
 def DBSCAN(arr, min_points, epsilon):
 
     clusters = Cluster(Array)
-    Cluster_num = 1
+    Cluster_num = 0
     
     for i, point in enumerate(arr):
         
+        # If the point has already been assigned a cluster, skip it
         if clusters.labels[i] != 0:
             continue
 
-        # get distances between point and all other points
+        # Get distances between point and all other points
+        # Function returns a tuple where the first element is an 
+        # array of distances and the second is the number of connections
         distances = get_distances(point, arr, epsilon)
-        #print(distances[1])
 
         if distances[1] < min_points:
             clusters.labels[i] = "N"
@@ -77,11 +120,20 @@ def DBSCAN(arr, min_points, epsilon):
             Cluster_num += 1
 
             # create cluster starting from the given point
-
-
-            #pass
+            create_cluster(point, arr, epsilon, min_points, clusters, Cluster_num)
 
     return clusters
+
+
+
+# Array = np.array([[0,0], [0,1], [0,2], [1,1], [5,1], [9,1], [10,0], [10,1], [10,2]])
+# clusters = Cluster(Array)
+
+# clusters[0] = clusters[5]
+
+# q = create_cluster(clusters.data[5], clusters.data, 3, 4, clusters, 'Q')
+# print(q)
+# print(clusters)
 
 
 Array = np.array([[0,0], [0,1], [0,2], [1,1], [5,1], [9,1], [10,0], [10,1], [10,2]])
@@ -94,60 +146,4 @@ Array = np.array([[0,0], [0,1], [0,2], [1,1], [5,1], [9,1], [10,0], [10,1], [10,
 min_points = 4
 epsilon = 3
 output = DBSCAN(Array, min_points, epsilon)
-#print(output)
-
-
-
-
-"""
-grow a cluster starting from some point and given a max distances and min number of points
-
-
-"""
-
-def create_cluster(point, arr, epsilon, min_points, cluster = []):
-
-    distances, connections = get_distances(point, arr, epsilon)
-
-    # base case, we run out of points
-    if connections == 0:
-        return
-    
-
-    # continue to jump to connecting points
-    # each time we jump, we shorten the input array
-    # remove the points that have been checked
-
-    # Filter the array down to only connecting points
-    # Remember that the indices for arr and distances correpsond to the same points
-    connecting_points = arr[distances[:,1]==1]
-
-    # Filter the array down to only connecting points and grab the indices
-    # We need the indices to act as a primary key between arr, distances, and the clusters object
-    #connecting_points = np.where(distances[:,1]==1)[0]
-
-    """
-    maybe I should assume that the point values can be used to link back to the clusters object.
-    
-    then I can just create a list of points and append after each loop
-    OR 
-    then I can search the cluster object for the corresponding point(s) and assign their labels
-    """
-
-
-    for p in connecting_points:
-        pass
-    
-    return connecting_points
-
-
-Array = np.array([[0,0], [0,1], [0,2], [1,1], [5,1], [9,1], [10,0], [10,1], [10,2]])
-
-q = create_cluster(Array[5], Array, 3, 4)
-print(q)
-
-q = Cluster(Array)
-print(q)
-# print(q[0])
-# q.labels[0] = 2
-# print(q)
+print(output)
